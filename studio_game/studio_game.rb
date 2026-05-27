c6950a9462bc96cf95bf74630fe208e4b03f4068
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-05-27 00:28:41 -0300

    add class notes

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,9 +1,43 @@
-# AULA 3
-puts "Let`s play a game! \n\n\t3\n\t2\n\t1\n\n"
-puts "🦜" * 5
+# AULA 4
 
-name = "Finn"
-health = 60
+# Variáveis e números são objetos e estão sujeitos a métodos.
+# Métodos são iniciados por pontos. Podem ser encadeáveis.
+# Métodos com exclamação no final alteram a variável original.
+# 8.*(2) é igual a 8*2 (atalho). Operações com números chamam métodos "por trás dos panos." 
 
-puts "#{name}`s health is #{health/9.0}"
+# Alguns exemplos:
 
+title = "Goonies"
+rank = 10
+
+title.length # Length é um método que pode ser chamado para objetos do tipo string. Assim como os a seguir.
+
+title.reverse
+
+title.upcase # Se colocarmos um "!" no fim do método, ele muda a variável.
+
+title.capitalize
+
+title.empty? # Aqui, o ponto de interrogação faz parte do método. Devolve um booleano.
+
+# "Por convenção, os métodos que terminam com um ponto de interrogação retornam um valor booleano, verdadeiro ou falso. Esses métodos são chamados de 'métodos predicados'".
+
+title.center(10)
+
+title.ljust(30, '.')
+
+title.ljust(30, '.').upcase # Concatenação de métodos.
+
+rank.to_f # Método que transforma um inteiro em um float.
+
+rank.to_s # Transforma em string.
+
+# Lembrando: multiplicar por um número chama um método "por trás dos panos".
+
+rank.even?
+
+rank.odd?
+
+# Como saber quais métodos são aplicáveis? Documentação. Nela, vemos quais os métodos e quais os argumentos possíveis.
+
+# EXERÍCIOS
