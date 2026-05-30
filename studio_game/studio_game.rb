904b9c63eab5b49f618fdf680db46b8a0d032717
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-05-30 18:45:56 -0300

    self

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,9 +1,21 @@
-# AULA 3
-puts "Let`s play a game! \n\n\t3\n\t2\n\t1\n\n"
-puts "🦜" * 5
+# AULA 5 - SELF
+# Métodos são sempre chamados em um objeto. Este objeto, no entanto, nem sempre está evidente.
+# O 'puts' é um método. Mas ele atua em que objeto?
+# No exemplo:
 
-name = "Finn"
-health = 60
+title = "Ghostbusters"
 
-puts "#{name}`s health is #{health/9.0}"
+rank = 9
 
+puts "#{title} has a rank of #{rank}"
+
+# A string é um argumento do método puts. Podemos colocar um parênteses:
+
+puts("#{title} has a rank of #{rank}")
+
+# Mas eles são comumente omitidos. Ainda não respondemos à questão: para qual objeto é chamado o método puts; isto é, o que vem "à esquerda do ponto"?
+# Sempre que não houver um receptor explícito no lado esquerdo do ponto, um receptor implícito ou objeto padrão é usado, e esse objeto é referenciado por uma VARIÁVEL chamada "self". Então, é como se usássemos explicitamente self como o receptor da chamada do método puts:
+
+self.puts("#{title} has a rank of #{rank}")
+
+# Em resumo: todo o método é aplicado em um objeto, mesmo que ele não esteja visível. 
