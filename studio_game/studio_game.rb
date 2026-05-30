f1376a2c6f1eb8712da3fc867e79c1412400f83c
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-05-30 20:54:25 -0300

    methods

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,68 +1,24 @@
-# AULA 4 - OBJECTS EVERYWHERE
+# AULA 6 - MAKING METHODS
+# O Ruby possui seus próprios métodos, mas não precisamos nos limitar a eles. Podemos criar nossos próprios métodos. Eles nos ajudam a automatizar tarefas.
 
-# Variáveis e números são objetos e estão sujeitos a métodos.
-# Métodos são iniciados por pontos. Podem ser encadeáveis.
-# Métodos com exclamação no final alteram a variável original.
-# 8.*(2) é igual a 8*2 (atalho). Operações com números chamam métodos "por trás dos panos." 
+def movie_info(title, rank)
+ "#{title} has a rank of #{rank}" 
+end
 
-# Alguns exemplos:
+# Em Ruby, os nomes dos métodos estão todos em letras minúsculas; e separamos as palavras com sublinhados. Sempre terminamos a definição do método com a palavra-chave "end". Métodos podem ter ou não argumentos.
+# Em vez de por o método "puts" dentro do nosso novo método, podemos imprimir o próprio método. O método é aplicado, implicitamente, no objeto "self", como vimos na última aula. Não precisamos digitá-lo.
+# Em Ruby, não precisamos retornar explicitamente um valor do método (com um "return"). Nesse caso, ao chamar o método, ele retornará sempre o valor da última expressão avaliada.
+# Ao chamar o método, que será aplicado em um objeto (mesmo que implícito, como no caso do "self"), precisamos passar seus argumentos, caso existam. O método pode ter valores padrão, de modo que, caso os argumentos não sejam passados, o valor definido como padrão será utilizado como argumento. Os valores dos parâmetros padrão podem ser derivados dos valores dos parâmetros anteriores.
+# De modo geral, os parâmetros precisam ser passados na ordem. Caso haja muitos argumentos, eles podem ser passados por meio de palavras-chave, não necessariamente na mesma ordem.
 
-title = "Goonies"
-rank = 10
+def movie_info(title:, rank:)
+ "#{title} has a rank of #{rank}" 
+end
 
-title.length # Length é um método que pode ser chamado para objetos do tipo string. Assim como os a seguir.
+puts movie_info(rank: 10, title: "Hereditary")
 
-title.reverse
+def movie_info(title:, rank: title.length/2)
+ "#{title} has a rank of #{rank}"
+end
 
-title.upcase # Se colocarmos um "!" no fim do método, ele muda a variável.
-
-title.capitalize
-
-title.empty? # Aqui, o ponto de interrogação faz parte do método. Devolve um booleano.
-
-# "Por convenção, os métodos que terminam com um ponto de interrogação retornam um valor booleano, verdadeiro ou falso. Esses métodos são chamados de 'métodos predicados'".
-
-title.center(10)
-
-title.ljust(30, '.')
-
-title.ljust(30, '.').upcase # Concatenação de métodos.
-
-rank.to_f # Método que transforma um inteiro em um float.
-
-rank.to_s # Transforma em string.
-
-# Lembrando: multiplicar por um número chama um método "por trás dos panos".
-
-rank.even?
-
-rank.odd?
-
-# Como saber quais métodos são aplicáveis? Documentação. Nela, vemos quais os métodos e quais os argumentos possíveis.
-
-# EXERÍCIOS
-
-name = "finn"
-health = 60
-
-puts "#{name.capitalize} has a health of #{health}"
-
-puts "#{name.upcase} has a health of #{health}"
-
-puts "#{name.capitalize} has a health of #{health}".center(50,"*")
-
-puts "#{name.capitalize.ljust(50, ".")} #{health} health"
-
-name2 = "    \n finn \t  "
-
-puts name2.strip() # Também pode ser "name2.strip", sem os parênteses.
-
-health2 = 123
-
-puts health2.to_s.reverse
-
-puts health2.to_s.reverse.to_i
-
-puts health2.to_s.reverse.to_i**2
-
-puts health2.to_s.reverse.to_i.pow(2)
+puts movie_info(title: "Hereditary")
