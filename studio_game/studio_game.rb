f78e9b6dcf74e913df66ad43ca7e5b5e00f78d99
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-06-02 20:03:32 -0300

    attribute

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,12 +1,14 @@
 class Player
-
+  attr_accessor :name
+  attr_reader :health
+  
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}"
+    "I'm #{@name} with a health of #{@health} and a score of #{score}"
   end
 
   def drain
@@ -16,18 +18,26 @@ class Player
   def boost
     @health += 15
   end
+
+  def score # Virtual attribute, no need for another instance variable ("@score")
+    @health + (@name).length
+  end  
 end
 
 player_1 = Player.new("finn", health = 60)
 puts player_1
+puts player_1.health
 
 player_2 = Player.new("lucy", health = 90)
 puts player_2
+puts player_2.health
 
 player_3 = Player.new("jase")
 puts player_3
+puts player_3.health
 
 player_4 = Player.new("alex", health = 125)
 puts player_4
 puts player_4.drain
 puts player_4.boost
+puts player_4.score
