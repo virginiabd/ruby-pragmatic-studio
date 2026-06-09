e25b509008a5ccd5c4aa7003094347d915000c34
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-06-09 16:03:08 -0300

    conditionals

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -19,7 +19,8 @@ class Player
     @health += 15
   end
 
-  def score # Virtual attribute, no need for another instance variable ("@score")
+  # Virtual attribute, no need for another instance variable ("@score")
+  def score 
     @health + (@name).length
   end  
 end
@@ -28,16 +29,29 @@ player_1 = Player.new("finn", health = 60)
 puts player_1
 puts player_1.health
 
-player_2 = Player.new("lucy", health = 90)
-puts player_2
-puts player_2.health
+number_rolled = rand(1..6)
 
-player_3 = Player.new("jase")
-puts player_3
-puts player_3.health
+case number_rolled
+when 1..2
+  player_1.drain
+  puts "#{player_1.name} got drained! =("
+when 3..4
+  puts "#{player_1.name} got skipped."
+else
+  player_1.boost
+  puts "#{player_1.name} got boosted! =)"
+end
 
-player_4 = Player.new("alex", health = 125)
-puts player_4
-puts player_4.drain
-puts player_4.boost
-puts player_4.score
+# player_2 = Player.new("lucy", health = 90)
+# puts player_2
+# puts player_2.health
+
+# player_3 = Player.new("jase")
+# puts player_3
+# puts player_3.health
+
+# player_4 = Player.new("alex", health = 125)
+# puts player_4
+# puts player_4.drain
+# puts player_4.boost
+# puts player_4.score
