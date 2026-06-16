864f76656095f06b9e54c3dc13d8f161c4cf4a26
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-06-15 21:27:07 -0300

    add all_tests

---
+++ test/studio_game/all_tests.rb
@@ -0,0 +1,2 @@
+require_relative "player_test"
+require_relative "game_test"
--- test/studio_game/player_test.rb
+++ test/studio_game/player_test.rb
@@ -8,4 +8,37 @@ class PlayerTest < Minitest::Test
     assert_equal "Finn", player.name
   end
 
+  def test_has_an_inital_health
+    player = Player.new("finn", 60)
+
+    assert_equal 60, player.health
+  end
+
+  def test_computes_score_as_sum_of_health_and_length_of_name
+    player = Player.new("finn", 60)
+
+    assert_equal 64, player.score
+  end
+
+  def test_has_a_string_representation
+    player = Player.new("finn", 60)
+
+    assert_equal "I'm Finn with a health of 60 and a score of 64", player.to_s
+  end
+
+  def test_boost_increases_health_by_15
+    player = Player.new("finn", 60)
+
+    player.boost
+
+    assert_equal 75, player.health
+  end
+
+  def test_drain_decreases_health_by_10
+    player = Player.new("finn", 60)
+
+    player.drain
+
+    assert_equal 50, player.health
+  end
 end
