8a65df5fea8ef6b23588d350296b1e94364045ab
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-06-16 17:12:00 -0300

    data and struct

--- lib/studio_game/game.rb
+++ lib/studio_game/game.rb
@@ -1,4 +1,17 @@
 class Game
+
+  Treasure = Data.define(:name, :points)
+
+  TREASURES = [
+    Treasure.new("pie", 10),
+    Treasure.new("coin", 25),
+    Treasure.new("flute", 50),
+    Treasure.new("compass", 65),
+    Treasure.new("key", 80),
+    Treasure.new("crown", 90),
+    Treasure.new("star", 100),
+  ]
+
   attr_reader :title, :players
 
   def initialize(title)
@@ -17,23 +30,31 @@ class Game
   def play(rounds = 1)
 
     puts "\nLet's play #{@title}!"
+
     puts "\nBefore playing:"
     puts @players
 
+    puts "\nThe following treasures can be found:"
+    TREASURES.each do |treasure|
+      puts "A #{treasure.name} is worth #{treasure.points}."
+    end
+
     1.upto(rounds) do |round|
+      puts "\nRound #{round}:"
       @players.each do |player|
-        puts "\nRound #{round}:"
         number_rolled = roll_die
         case number_rolled
         when 1..2
           player.drain
-          puts "\n#{player.name} got drained! =("
+          puts "#{player.name} got drained! =("
         when 3..4
-          puts "\n#{player.name} got skipped."
+          puts "#{player.name} got skipped."
         else
           player.boost
-          puts "\n#{player.name} got boosted! =)"
+          puts "#{player.name} got boosted! =)"
         end
+        treasure = TREASURES.sample
+        puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
       end
     end
 
