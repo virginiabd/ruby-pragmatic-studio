6c19c3728822bdf1a178c4da601ff6e95db056d4
Virgínia Dalvi <virginiadalvi@gmail.com>
2026-06-23 20:18:29 -0300

    user input

--- studio_game.rb
+++ studio_game.rb
@@ -11,16 +11,32 @@ game.add_player(player_1)
 game.add_player(player_2)
 game.add_player(player_3)
 game.add_player(player_4)
-game.play(3)
-game.print_stats
 
-player_5 = Player.new("alvin")
-player_6 = Player.new("simon")
-player_7 = Player.new("theodore")
+loop do
+  print"\n How many game rounds? ('Quit/exit' to exit). "
+  answer = gets.chomp.downcase
 
-game_2 = Game.new("Chipmunks")
-game_2.add_player(player_5)
-game_2.add_player(player_6)
-game_2.add_player(player_7)
-game_2.play
-game.print_stats
+  case answer
+  when /^\d+$/
+    game.play(answer.to_i)
+  when "quit", "exit"
+    game.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit/exit'."
+  end
+end
+
+# game.play(3)
+# game.print_stats
+
+# player_5 = Player.new("alvin")
+# player_6 = Player.new("simon")
+# player_7 = Player.new("theodore")
+
+# game_2 = Game.new("Chipmunks")
+# game_2.add_player(player_5)
+# game_2.add_player(player_6)
+# game_2.add_player(player_7)
+# game_2.play
+# game_2.print_stats
